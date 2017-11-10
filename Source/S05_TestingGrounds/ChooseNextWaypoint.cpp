2d84be47592fc9ad8b5e8f1b0ed746bf522e029d
Giovanni Giacomo <giovanni@unitool.com.br>
2017-11-10 21:34:53 -0200

    Switched To Visual Studio Code & Finished C++ Next Waypoint Task

--- Source/S05_TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/S05_TestingGrounds/ChooseNextWaypoint.cpp
@@ -2,12 +2,25 @@
 
 #include "ChooseNextWaypoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+#include "PatrollingGuard.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent &OwnerComp, uint8 *NodeMemory)
 {
+    // Get Patrol Points
+    AAIController* Controller = OwnerComp.GetAIOwner();
+    APawn* Pawn = Controller->GetPawn();
+    APatrollingGuard* Guard = Cast<APatrollingGuard>(Pawn);
+    TArray<AActor*> PatrolPoints = Guard->PatrolPointsCpp;
+    
+    // Set Next Waypoint
     UBlackboardComponent* Component = OwnerComp.GetBlackboardComponent();
     int Index = Component->GetValueAsInt(IndexKey.SelectedKeyName);
+    Component->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
     
-    UE_LOG(LogTemp, Warning, TEXT("Waypoint Index: %i"), Index);
+    // Cycle Index
+    int NextIndex = (Index + 1) % PatrolPoints.Num();
+    Component->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+    
     return EBTNodeResult::Succeeded;
 }
